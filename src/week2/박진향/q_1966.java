01d0734960e2f28ec085ca0b87047a19d77850f6
Jhyangee <wlsgid916@gmail.com>
2022-06-04 23:49:44 +0900

    week2

---
+++ src/week2/박진향/q_1966.java
@@ -0,0 +1,70 @@
+package week2.박진향;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
+import java.util.LinkedList;
+
+/* 백준 1966  프린터 큐 */
+public class q_1966 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        while ( T > 0) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            LinkedList<int[]> q = new LinkedList<>();
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < N; i++) {
+                q.offer(new int[] { i, Integer.parseInt(st.nextToken()) });
+            }
+
+            int count = 0;
+
+            while (!q.isEmpty()) {
+
+                int[] front = q.poll();
+                boolean isMax = true;
+
+                for(int i = 0; i < q.size(); i++) {
+
+                    if(front[1] < q.get(i)[1]) {
+
+                        q.offer(front);
+                        for(int j = 0; j < i; j++) {
+                            q.offer(q.poll());
+                        }
+                        isMax = false;
+                        break;
+                    }
+                }
+
+
+                if(isMax == false) {
+                    continue;
+                }
+                count++;
+
+                if(front[0] == M) {
+                    break;
+                }
+
+            }
+            sb.append(count).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
---
+++ src/week2/박진향/q_5430.java
@@ -0,0 +1,18 @@
+package week2.박진향;
+
+/* 백준 5430 AC */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.ArrayDeque;
+
+public class q_5430 {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
