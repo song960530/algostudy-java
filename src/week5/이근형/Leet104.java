a6a3bc2cc2f40e739bc8af64a723166223762ae3
devraphy <devraphy@gmail.com>
2022-06-25 16:32:23 +0900

    solution changed for Leet104

--- src/week5/이근형/Leet104.java
+++ src/week5/이근형/Leet104.java
@@ -5,32 +5,43 @@ import java.util.Queue;
 
 public class Leet104 {
 
-    static int maxDepth(TreeNode root) {
-
-        if (root == null) return 0;
-
-        Queue<TreeNode> que = new LinkedList<>();
-        que.offer(root);
-        int depth = 0;
-
-        while (!que.isEmpty()) {
-            depth++;
-            int size = que.size();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode curn = que.poll();
-
-                if (curn.left != null) {
-                    que.offer(curn.left);
-                }
-
-                if (curn.right != null) {
-                    que.offer(curn.right);
-                }
-            }
-        }
-        return depth;
+    public int maxDepth(TreeNode root) { // DFS 방식
+        if(root == null) return 0;
+        return dfs(root, 0);
     }
+
+    public int dfs(TreeNode curn, int depth) {
+        if(curn == null) return depth;
+        depth++;
+        return Math.max(dfs(curn.left, depth), dfs(curn.right, depth));
+    }
+
+//    static int maxDepth(TreeNode root) { // BFS 방식
+//
+//        if (root == null) return 0;
+//
+//        Queue<TreeNode> que = new LinkedList<>();
+//        que.offer(root);
+//        int depth = 0;
+//
+//        while (!que.isEmpty()) {
+//            depth++;
+//            int size = que.size();
+//
+//            for (int i = 0; i < size; i++) {
+//                TreeNode curn = que.poll();
+//
+//                if (curn.left != null) {
+//                    que.offer(curn.left);
+//                }
+//
+//                if (curn.right != null) {
+//                    que.offer(curn.right);
+//                }
+//            }
+//        }
+//        return depth;
+//    }
 
     static class TreeNode {
         int val;
