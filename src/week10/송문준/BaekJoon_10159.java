f63c0406173279da409d8774f1dcf0423c393025
SongMunJun <song960530@naver.com>
2022-08-04 23:58:13 +0900

    feat: 10159 풀이 추가

---
+++ src/week10/송문준/BaekJoon_10159.java
@@ -0,0 +1,142 @@
+package week10.송문준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class BaekJoon_10159 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] result;
+
+    public static void main(String[] args) throws IOException {
+        // 첫번째줄 read
+        int[] cmd = readLine();
+
+        // node 생성
+        List<Node> nodes = makeNodes(cmd[0]);
+
+        // 결과담을 배열 초기화
+        result = new int[cmd[0] + 1];
+
+        // 두번째줄 read
+        cmd = readLine();
+
+        // 링크 연결
+        linkEachNode(cmd[0], nodes);
+
+        Queue<Node> queue = new LinkedList<>();
+        for (Node node : nodes) {
+            queue.offer(node);
+
+            // 정방향 노드 탐색
+            bfsLinked(queue);
+
+            tmpVisitClear(nodes);
+
+            // 역방향(반대방향) 노드 탐색
+            bfsTmpLinked(queue);
+
+            recordResult(nodes, node);
+
+            allVisitClear(nodes);
+        }
+
+        for (int i : result) {
+            System.out.println(i);
+        }
+    }
+
+    private static void recordResult(List<Node> nodes, Node node) {
+        int count = nodes.size();
+        for (Node node1 : nodes) {
+            if (node1.visit) count--;
+        }
+        result[node.idx] = count;
+    }
+
+    private static void bfsTmpLinked(Queue<Node> queue) {
+        while (!queue.isEmpty()) {
+            Node poll = queue.poll();
+
+            if (poll.tmpVisit)
+                continue;
+
+            for (Node linkNode : poll.converseLinked) {
+                queue.offer(linkNode);
+            }
+
+            poll.tmpVisit = true;
+            poll.visit = true;
+        }
+    }
+
+    private static void bfsLinked(Queue<Node> queue) {
+        while (!queue.isEmpty()) {
+            Node poll = queue.poll();
+
+            if (poll.tmpVisit)
+                continue;
+
+            for (Node linkNode : poll.linked) {
+                queue.offer(linkNode);
+            }
+
+            poll.tmpVisit = true;
+            poll.visit = true;
+        }
+    }
+
+    private static void tmpVisitClear(List<Node> nodes) {
+        nodes.forEach(node -> {
+            node.tmpVisit = false;
+        });
+    }
+
+    private static void allVisitClear(List<Node> nodes) {
+        nodes.forEach(node -> {
+            node.visit = false;
+            node.tmpVisit = false;
+        });
+    }
+
+    private static void linkEachNode(int count, List<Node> nodes) throws IOException {
+        for (int i = 0; i < count; i++) {
+            int[] cmd = readLine();
+
+            nodes.get(cmd[0] - 1).linked.add(nodes.get(cmd[1] - 1)); // 정방향 연결
+            nodes.get(cmd[1] - 1).converseLinked.add(nodes.get(cmd[0] - 1)); // 반대방향 연결
+        }
+    }
+
+    private static List<Node> makeNodes(int count) {
+        return IntStream.iterate(1, i -> i + 1)
+                .mapToObj(i -> new Node(i))
+                .limit(count)
+                .collect(Collectors.toList());
+    }
+
+    private static int[] readLine() throws IOException {
+        return Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    static class Node {
+        int idx;
+        List<Node> linked;
+        List<Node> converseLinked;
+        boolean visit;
+        boolean tmpVisit;
+
+        public Node(int idx) {
+            this.idx = idx;
+            this.linked = new LinkedList<>();
+            this.converseLinked = new LinkedList<>();
+        }
+    }
+
+}
