3dace5e86b15c58a20f0ce8bdbe60b6f19604a81
SongMunJun <song960530@naver.com>
2022-08-12 22:13:19 +0900

    refactor: 1719번 수정

--- src/week10/송문준/BaekJoon_1719.java
+++ src/week10/송문준/BaekJoon_1719.java
@@ -3,137 +3,94 @@ package week10.송문준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
 
+// 못품
 public class BaekJoon_1719 {
-
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static Queue<Map<Integer, Integer>> queue = new LinkedList<>();
-    static Map<Integer, Map<Integer, Integer>> allLinks; // key -> 시작노드, value -> 연결노드,가중치
-    static boolean[] visit;
-    static Integer[][] route;
-    static Integer[][] min;
 
     public static void main(String[] args) throws IOException {
         // 첫번째줄 read
         int[] cmd = readLine();
 
-        // 배열 초기화
-        makeArray(cmd[0]);
+        // a -> b까지 가는 경로를 저장하는 배열
+        // 최종결과물이 된다
+        int[][] route = new int[cmd[0] + 1][cmd[0] + 1];
 
-        // 경로 및 가중치 저장
-        makeLinks(cmd[1]);
+        // 노드에서 노드르 이동하는 간선정보를 저장하는 배열
+        int[][] graph = initMaxValue(cmd[0] + 1, 999999);
 
-        sortAsc();
+        // 입력값 받아서 그래프 그리기
+        for (int i = 0; i < cmd[1]; i++) {
+            int[] cmd2 = readLine();
+            graph[cmd2[0]][cmd2[1]] = cmd2[2];
+            graph[cmd2[1]][cmd2[0]] = cmd2[2];
 
-        for (Map.Entry<Integer, Map<Integer, Integer>> entry : allLinks.entrySet()) {
-            Integer startNode = entry.getKey();
+            route[cmd2[0]][cmd2[1]] = cmd2[1];
+            route[cmd2[1]][cmd2[0]] = cmd2[0];
+        }
 
-            visit = new boolean[cmd[0] + 1];
-            queue.offer(entry.getValue());
+        floydWarshall(route, graph);
 
-            boolean first = true;
+        // 결과 출력
+        print(route);
+    }
 
-            while (!queue.isEmpty()) {
-                Map<Integer, Integer> map = queue.poll();
-                Integer nowNode = findKey(map);
-
-                // 이미 방문했으면 pass
-                if (visit[nowNode])
-                    continue;
-
-                for (Map.Entry<Integer, Integer> link : map.entrySet()) {
-                    Integer endNode = link.getKey();
-                    Integer weight = link.getValue();
-
-                    if (startNode == 6 && endNode == 1) {
-                        System.out.println();
+    private static void floydWarshall(int[][] route, int[][] graph) {
+        for (int k = 1; k < graph.length; k++) { // 경유지
+            for (int i = 1; i < graph.length; i++) { // 출발지
+                for (int j = 1; j < graph.length; j++) { // 도착지
+                    int weight = graph[i][k] + graph[k][j];
+                    if (weight < graph[i][j]) {
+                        graph[i][j] = weight;
+                        route[i][j] = route[k][j];
                     }
-
-                    if (first) {
-                        min[startNode][endNode] = weight;
-                        route[startNode][endNode] = endNode;
-                    } else {
-                        if (min[startNode][endNode] == null || min[startNode][endNode] > min[startNode][nowNode] + weight) {
-                            min[startNode][endNode] = min[startNode][nowNode] + weight;
-
-                            Integer tmp = nowNode;
-
-                            if(tmp == )
-                            route[startNode][endNode] = route[startNode][nowNode];
-                        }
-                    }
-
-                    queue.offer(allLinks.get(endNode));
                 }
-
-                first = false;
-
-                // 방문 헀으니 true
-                visit[nowNode] = true;
-            }
-
-        }
-        wrapUp();
-
-        System.out.println(print());
-    }
-
-    private static String print() {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 1; i < route.length; i++) {
-            for (int j = 1; j < route[i].length; j++) {
-                sb.append((route[i][j] != null ? route[i][j] : "-") + " ");
-            }
-            sb.append("\n");
-        }
-
-        return sb.toString();
-    }
-
-    private static void sortAsc() {
-        for (Map<Integer, Integer> value : allLinks.values()) {
-
-        }
-    }
-
-    private static void wrapUp() {
-        for (int i = 0; i < route.length; i++) {
-            route[i][i] = null;
-        }
-    }
-
-    private static Integer findKey(Map<Integer, Integer> map) {
-        for (Map.Entry<Integer, Map<Integer, Integer>> entry2 : allLinks.entrySet()) {
-            if (entry2.getValue() == map) {
-                return entry2.getKey();
             }
         }
-        return null;
     }
 
-    private static void makeLinks(int loopCount) throws IOException {
-        allLinks = new HashMap<>();
+    private static int[][] initMaxValue(int count, int value) {
+        int[][] result = new int[count][count];
 
-        // 양방향 가중치 연결
-        for (int i = 0; i < loopCount; i++) {
-            int[] cmd = readLine();
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                if (i == j) {
+                    result[i][j] = 0;
+                } else {
+                    result[i][j] = value;
+                }
+            }
+        }
 
-            Map<Integer, Integer> map = allLinks.getOrDefault(cmd[0], new TreeMap<>());
-            map.put(cmd[1], cmd[2]);
-            allLinks.put(cmd[0], map);
+        return result;
+    }
 
-            Map<Integer, Integer> map2 = allLinks.getOrDefault(cmd[1], new TreeMap<>());
-            map2.put(cmd[0], cmd[2]);
-            allLinks.put(cmd[1], map2);
+    private static int[][] fillGraph(int[] cmd) throws IOException {
+        int[][] graph = initMaxValue(cmd[0] + 1, Integer.MAX_VALUE);
+
+        for (int i = 0; i < cmd[1]; i++) {
+            int[] cmd2 = readLine();
+            graph[cmd2[0]][cmd2[1]] = cmd2[2];
+            graph[cmd2[1]][cmd2[0]] = cmd2[2];
+        }
+
+        return graph;
+    }
+
+    private static void print(int[][] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            if (i == 0)
+                continue;
+            for (int j = 0; j < graph[0].length; j++) {
+                if (j == 0)
+                    continue;
+                System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 
-    private static void makeArray(int nodeCount) {
-        route = new Integer[nodeCount + 1][nodeCount + 1];
-        min = new Integer[nodeCount + 1][nodeCount + 1];
-    }
 
     private static int[] readLine() throws IOException {
         return Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
